37fdc1c2b102383f669904984f836ffe8c8a717a
coding-club-iimt <95184981+coding-club-iimt@users.noreply.github.com>
2022-01-08 11:53:16 +0530

    Update logic.php

--- logic.php
+++ logic.php
@@ -1,35 +1,26 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
      //get previous data
-     function get_data() {
-        $name = $_POST['name'];
+    
+       
+        $topic = $_POST['topic'];
+        $difficulty = $_POST['difficulty'];
+
+        header('Location: //https://practice.geeksforgeeks.org/explore/?difficulty%5B%5D='$difficulty'&page=1&category%5B%5D='$topic');
+
+
+        
        
    
         
-            $current_data=file_get_contents("Problems.json");
-            $array_data=json_decode($current_data, true);
-                               
-            $extra=array(
-                'name' => $_POST['name'],
-                'topic' => $_POST['topic'],
-                'link' => $_POST['link'],
-                'difficulty' => $_POST['difficulty']
-            );
-            $array_data[]=$extra;
-            //echo "file exist<br/>";
-            return json_encode($array_data);
+          
         
         
-    }
+    
 
     //write data to file
    
-        if(file_put_contents('Problems.json', get_data())) {
-            echo "Data successfully saved";
-        }
-        else{
-            echo "error";
-        }
+      
     
 
 
